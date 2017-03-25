7f22e20a87187ed890343fe71f89e323d707b762
k0rg0th <barbaria.korgoth@gmail.com>
2017-03-25 21:53:55 +0100

    remove exceced lines

--- GitJavaProject/src/com/beauce/git/GitTest.java
+++ GitJavaProject/src/com/beauce/git/GitTest.java
@@ -3,7 +3,5 @@ package com.beauce.git;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		System.out.println("Second Line");
-		System.out.println("Third Line");
 	}
 }
